24777c52d7cdcd535fd6b556167b98f43fd905bd
JosephMMueller15 <35577472+JosephMMueller15@users.noreply.github.com>
2018-01-23 15:52:46 -0600

    Add files via upload

---
+++ printName.c
@@ -0,0 +1,12 @@
+/***********************************
+*Name: Joseph M Mueller
+*Date: 1-22-18
+*Program Detail: This program will output the name so that it can be used in GitHub
+***********************************/
+
+#include<stdio.h>
+
+int main(int argc, char **argv){
+	printf("Joseph Mueller");
+return 0;
+}
